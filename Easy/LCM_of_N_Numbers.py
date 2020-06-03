419bad891355ad55eaf91cffcefcc57554f895ac
Hemant Singh <singh.hemant9583@gmail.com>
2020-06-03 12:26:41 +0530

    Easy/LCM_of_N_Numbers.py

---
+++ Easy/LCM_of_N_Numbers.py
@@ -0,0 +1,19 @@
+import math
+
+def GCD(a,b,origa,origb):
+    if a%b == 0:
+        return int(int(origa*origb)/int(b)) 
+    else:
+        return GCD(b,a%b,origa,origb)
+
+N = list(map(int,input().split()))
+
+a = N[0]
+b = N[1]
+li = GCD(a,b,a,b)
+
+for i in range(2, len(N)):
+
+    li = GCD(li,N[i],li,N[i])
+
+print('LCM:',li)
