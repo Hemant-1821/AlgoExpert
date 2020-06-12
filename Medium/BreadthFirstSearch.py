ce7c0a12192848c76c85e0f1dcf2f8b95a19da12
Hemant Singh <singh.hemant9583@gmail.com>
2020-06-12 16:57:54 +0530

    Medium/BreadthFirstSearch.py

---
+++ Medium/BreadthFirstSearch.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self,name):
+        self.name = name
+        self.children = []
+
+    def addChild(self,name):
+        self.children.append(Node(name))
+
+    # Complexities O(V+E) TIME | O(V) SPACE
+    def breadthFirstSearch(self, array):
+        queue = [self]
+        while len(queue) > 0:
+            current = queue.pop(0)
+            array.append(current.name)
+            for child in current.children:
+                queue.append(child)
+
+        return queue
